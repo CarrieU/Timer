56e9df90ebd8c6a493c6390df5f333bf4911c66e
Carrie <CarrieU1990@gmail.com>
2018-05-03 21:40:00 +0800

    Timer list
    Create list page

--- pages/timer/index.js
+++ pages/timer/index.js
@@ -1,12 +1,58 @@
-var timer = require('timer.json');
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list :timer,
+    timerlist: [{
+      "timerId": "1",
+      "timerName": "test timer",
+      "loop": "5",
+      "description": "测试定时器",
+      "open":true,
+      "timerTask": [{
+        "taskId": "11",
+        "taskName": "step01",
+        "duration": "30000",
+        "order": "1"
+      }, {
+        "taskId": "12",
+        "taskName": "step02",
+        "duration": "5000",
+        "order": "2"
+      }, {
+        "taskId": "13",
+        "taskName": "step03",
+        "duration": "10000",
+        "order": "3"
+      }]
+    }, {
+      "timerId": "2",
+      "timerName": "test timer 02",
+      "loop": "5",
+      "description": "测试定时器02",
+      "open": true,
+      "timerTask": [
+        {
+          "taskId": "21",
+          "taskName": "step01-02",
+          "duration": "30000",
+          "order": "1"
+        },
+        {
+          "taskId": "22",
+          "taskName": "step02-02",
+          "duration": "5000",
+          "order": "2"
+        },
+        {
+          "taskId": "23",
+          "taskName": "step03-02",
+          "duration": "10000",
+          "order": "3"
+        }
+      ]
+    }],
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
@@ -18,68 +64,7 @@ Page({
     duration: 1000
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
-    wx.request({
-      url: '/',
-      headers: "",
-      success: function(res){
-        that.setData({
-          timer:res.data.timers
-        });
-      }
-    })
-  },
+  kindToggle:function(e){
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
   }
 })
